6568cc2ad7ef8035524af1386aa59876ce2d7140
Jacek <j.labudda@dock.codes>
2026-03-12 11:10:36 +0100

    updated

--- wp_docker/templates/theme-tailwind/tailwind.config.js
+++ wp_docker/templates/theme-tailwind/tailwind.config.js
@@ -12,7 +12,6 @@ module.exports = {
         },
     },
     plugins: [
-        require('@tailwindcss/typography'),
-        require('tailwindcss-motion')
+        require('@tailwindcss/typography')
     ]
 }
