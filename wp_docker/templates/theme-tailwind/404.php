4ed527c444358bd4a68112f2fbf0c6248f8e9e64
Jacek <j.labudda@dock.codes>
2026-03-16 18:53:10 +0100

    improved

--- wp_docker/templates/theme-tailwind/404.php
+++ wp_docker/templates/theme-tailwind/404.php
@@ -1,13 +1,13 @@
 <?php get_header(); ?>
-    <section class="py-32 text-center">
-        <h1 class="text-6xl font-bold mb-6">
-            404
-        </h1>
-        <p class="text-gray-600 mb-8">
-            Page not found
-        </p>
-        <a href="/" class="bg-black text-white px-6 py-3 rounded">
-            Back to homepage
-        </a>
-    </section>
+<section class="py-32 text-center">
+    <h1 class="text-6xl font-bold mb-6">
+        404
+    </h1>
+    <p class="text-gray-600 mb-8">
+        Page not found
+    </p>
+    <a href="/" class="bg-black text-white px-6 py-3 rounded">
+        Back to homepage
+    </a>
+</section>
 <?php get_footer(); ?>
--- wp_docker/templates/theme-tailwind/front-page.php
+++ wp_docker/templates/theme-tailwind/front-page.php
@@ -4,13 +4,22 @@
  */
 
 get_header(); ?>
-    <main>
-        <?php
-        if (have_posts()) :
-            while (have_posts()) : the_post();
-                get_template_part('template-parts/content', 'page');
-            endwhile;
-        endif;
-        ?>
-    </main>
+<main>
+    <?php
+    if (have_posts()) :
+        while (have_posts()) : the_post();
+            $page_sections = get_field('page_sections');
+            if ($page_sections) {
+                foreach ($page_sections as $section) {
+                    $section_name = str_replace('_', '-', $section['acf_fc_layout']);
+                    $file = locate_template('template-parts/sections/' . $section_name . '.php');
+                    if ($file) {
+                        get_template_part('template-parts/sections/' . $section_name, null, ['section_data' => $section]);
+                    }
+                }
+            }
+        endwhile;
+    endif;
+    ?>
+</main>
 <?php get_footer(); ?>
